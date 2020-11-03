8cd7c0f62990c192897f3fd2f60877dc885c850a
Mayur Ingle <mingle@altimetrik.com>
2020-11-03 19:28:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import BulidTodo from "./components/bulid-todo.component";
+import ModifyTodo from "./components/modify-todo.component";
+import TodosList from "./components/todos-list.component";
+
+import logo from "./Logo-onUrDesk.jpg";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="https://onurdesk.com">
+              <img src={logo} width="30" height="30" alt="onurdesk.com" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collpase nav-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/bulid" className="nav-link">Bulid Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <Route path="/" exact component={TodosList} />
+          <Route path="/modify/:id" component={ModifyTodo} />
+          <Route path="/bulid" component={BulidTodo} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
